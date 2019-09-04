d7d532dbc592807e93c73851b1c729ebf33f8ef4
Quest Henkart <qhenkart@gmail.com>
2019-09-04 14:34:44 +0300

    docs

--- objectid.go
+++ objectid.go
@@ -1,18 +1,23 @@
 // Package oid provides an easy to use/integrate abstraction layer between your code and the primitive package of the official mongo driver
 //
-// helps to bridge the gap between the new [mongo-go-driver](https://github.com/mongodb/mongo-go-driver) and previous community run drivers such as [mgo](https://github.com/globalsign/mgo).
+// helps to bridge the gap between the new mongo-go-driver and previous community run drivers such as the mgo package
 //
 // For people not migrating, this also helps cover some of the pitfalls and frustrations of mongo-go-driver's primitive package which is extremely brittle, leaks too much of the driver to the clients of the API, and can even cause unexpected panics when unmarshalling
 //
 // This package follows the community run driver standard of using strings to represent objectIDs instead of [12]bytes, allowing for a much smoother development experience.
 //
-// ##Features
+// Features
 //
 // 1. This package automatically converts all oid.ObjectID types into primitive.ObjectIDs when marshalling or unmarshalling into bson
+//
 // 2. no panics on JSON unmarshalling
+//
 // 3. uses string types to avoid the driver bleeding to the API and give more control to the dev
+//
 // 4. fixes vet errors that was rampant in the community drivers
+//
 // 5. combines the best features of community drivers and the primitive package
+//
 // 6. makes migrating significantly easier
 package oid
 
